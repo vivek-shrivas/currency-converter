cbcb3cd358308646eac0ebfa886defc4f3786fe0
vivek-shrivas <vivekshrivas347@gmail.com>
2024-04-27 23:11:51 +0530

    made a currency converter that converts inr to usd

---
+++ client/currency-converter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Converter from './components/Converter.jsx'
+
+function App() {
+  return (<>
+    <div>MY REACT APP</div>
+    <Converter  />
+    </>
+  )
+}
+
+export default App
